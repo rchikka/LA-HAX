bf58702d443dfe664959d5f2aa61f080bcf7fb9d
rchikka11@gmail.com <rchikka@LAPTOP-A71JMUN7.localdomain>
2019-03-31 04:33:32 -0700

    update final.py

--- backend/final.py
+++ backend/final.py
@@ -1,13 +1,61 @@
 import io
 import os
 import cv2
-
+import numpy as np
 
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
 
+#route =  bananaIMG.jpg
 route = '/home/rchikka/gcloud/visionex/a-Ripening-Stages-of-Banana-Fruit-1-7-Completely-unripe-to-completely-ripen.png'
+#route = '/home/rchikka/gcloud/visionex/190683-131-E796F1EE.jpg'
+
+def buffer(x, y):
+    if (x + 50 > y) or (x - 50 < y):
+        return True
+
+def colorAnalyze(arc):
+    # arc = np.load(route)
+    valid_count = []
+    blu_conch = 0
+    red_conch = 0
+    gre_conch = 0
+    whitespace = 0
+    black = 0
+    yellow_conch = 0
+    reference = int(arc.shape[1] / 2)
+
+    for n in range(0, arc.shape[0]):
+        for m in range(0, arc.shape[1]):
+            for q in range(0, 3):
+                if (arc[n][m][q] != arc[0][reference][q]):
+                    valid_count.append(arc[n][m])
+    #print("valid pixels: " + str(len(valid_count)))
+    for n in valid_count:
+        if n[0] > 230 and n[1] > 230 and n[2] > 220:
+            whitespace = whitespace + 1
+        if n[0] < 120 and n[1] < 120 and n[2] < 120:
+            black = black + 1
+        elif n[1] > n[2] and n[1] > n[0]:
+            gre_conch = gre_conch + 1
+        elif buffer(n[0], n[1]) == True:
+            yellow_conch = yellow_conch + 1
+        elif n[2] > n[1] and n[2] > n[0]:
+            blu_conch = blu_conch + 1
+        if n[0] > n[1] + 50 and n[0] > n[2]:
+            red_conch = red_conch + 1
+
+    comp = len(valid_count) - whitespace
+
+    if (gre_conch / comp) > yellow_conch / comp:
+        return 0  # print("unripe")
+    elif (black / comp) > (0.5 * yellow_conch / comp):
+        return 2  # print("overripe")
+    elif (red_conch / comp) + 0.1 > (0.5 * yellow_conch / comp):
+        return 1  # print("soon to expire: eat soon")
+    else:
+        return 1  # print("store ready")
 
 def markBackground(filename):
     # == Parameters =======================================================================
@@ -91,8 +139,11 @@ def markBackground(filename):
 def cropBox(img, v):
     # img is the full image
     # v is a list of vertices, [[x,y], [x,y]]
-    x, y = v[0]
-    h, w = abs(y - v[1][1]), abs(x - v[1][0])
+    x, y = v[0][0]+2, v[0][1]+2
+    h, w = abs(y - v[1][1] - 2), abs(x - v[1][0] - 2)
+    
+    #x, y = v[0]
+    #h, w = abs(y-v[1][1]), abs(x - v[1][0])
     img = cv2.imread(img)
     crop_img = img[y:y + h, x:x + w].copy()
     # cv2.imshow("cropped", crop_img)
@@ -148,14 +199,29 @@ def localize_objects(path):
             		coords.append([int(vertex.x * img.shape[1]), int(vertex.y * img.shape[0])])
     return coords
 
-if(not label_check(route)): 
-	print("oh no")
-	#error message
+def main():
+	if(not label_check(route)): 
+		return 3
+		# no  banana was found
+		#print("oh no")
+		#error message
 
-imagelist = []
-vertices = localize_objects(route)
-#for i in range(len(vertices)):
-#	print("x-vertex: " + str(vertices[i][0]))
-#	print("y-vertex: " + str(vertices[i][1]))
-for i in range(len(vertices)-2):
-	imagelist.append(cropBox(route, [vertices[i], vertices[i+2]]))
+	imagelist = []
+	vertices = localize_objects(route)
+	#for i in range(len(vertices)):
+	#	print("x-vertex: " + str(vertices[i][0]))
+	#	print("y-vertex: " + str(vertices[i][1]))
+	for i in range(len(vertices)-2):
+		imagelist.append(cropBox(route, [vertices[i], vertices[i+2]]))
+
+	tally = [0, 0, 0]
+
+	for img in imagelist:
+		tally[colorAnalyze(img)] += 1
+
+	spot = tally.index(max(tally))
+	#print(tally)
+	#print(spot)
+	#return spot
+
+main()
